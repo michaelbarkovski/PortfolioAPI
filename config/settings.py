36955c0b53250541dd6a25eeba6ab89aa179982d
Michael Barkovski <michaelbarkovski@Michaels-Air.lan>
2026-03-10 10:00:59 +0000

    API rate limiting added

--- config/settings.py
+++ config/settings.py
@@ -55,6 +55,15 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": (
         "rest_framework.permissions.IsAuthenticatedOrReadOnly",
     ),
+
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "30/hour",
+        "user": "200/hour",
+    },
 }
 
 SIMPLE_JWT = {
